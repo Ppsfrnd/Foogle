d7e5772982d91c072f1da47e225b086027f5511a
Ppsfrnd <p.prasanth22@gmail.com>
2018-02-23 12:38:50 +0530

    mockito test added

--- FOOGLE/src/test/java/com/tavant/FriendsFinderApplicationTests.java
+++ FOOGLE/src/test/java/com/tavant/FriendsFinderApplicationTests.java
@@ -1,16 +1,49 @@
 package com.tavant;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.*;
+import org.mockito.MockitoAnnotations;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tavant.model.Friend;
+import com.tavant.repository.FriendRepository;
+import com.tavant.service.FriendServiceImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FriendsFinderApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Mock
+	private FriendRepository friendRepository;
+	
+	@InjectMocks
+	private FriendServiceImpl friendService;
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
 	}
+	
+	
+	@Test
+	public void testGetToDoById(){
+		Friend friend = new Friend();
+		friend.setId(3);
+		when(friendRepository.displayFriendById(3)).thenReturn(friend);
+		
+		Friend result = friendService.displayFriendById(3);
+		verify(friendRepository).displayFriendById(3);
+		assertEquals(3, result.getId());
+	}
+	
 
 }
+
